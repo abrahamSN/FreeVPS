36c8eac1712c5c5ca58e416eafb8f4accf0fa68a
Abraham Setya Nugraha <abrahamsn.mayer@gmail.com>
2025-01-29 03:59:35 +0700

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -21,7 +21,7 @@ fi
 echo "### Install ngrok ###"
 
 wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar -xvzf ~/Downloads/ngrok-v3-stable-linux-amd64.tgz
+tar -xvzf ngrok-v3-stable-linux-amd64.tgz
 chmod +x ./ngrok
 
 echo "### Update user: $USER password ###"
